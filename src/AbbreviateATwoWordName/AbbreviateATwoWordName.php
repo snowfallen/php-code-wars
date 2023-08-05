1bf2d0822bd7073820040a570b6eae303fd9c4bb
snowfallen <bondarenkoov1998@gmail.com>
2023-08-05 23:46:46 +0200

    add Abbreviate A Two Word Name task solution

---
+++ src/AbbreviateATwoWordName/AbbreviateATwoWordName.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PhpCodeWars\AbbreviateATwoWordName;
+
+class AbbreviateATwoWordName
+{
+    /**
+     * @param string $name
+     * @return string
+     */
+    final public function abbreviateName(string $name): string
+    {
+        $words = explode(' ', $name);
+        return strtoupper($words[0][0] . $words[1][0]);
+    }
+}
