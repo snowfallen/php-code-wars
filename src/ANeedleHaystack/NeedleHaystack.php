7417e5b1cae1fc95f7d2ff1e7bd3c5e3d4245c70
snowfallen <bondarenkoov1998@gmail.com>
2023-08-25 11:12:57 +0200

    add Needle Haystack task solution shorter version

--- src/ANeedleHaystack/NeedleHaystack.php
+++ src/ANeedleHaystack/NeedleHaystack.php
@@ -6,14 +6,6 @@ class NeedleHaystack
 {
     public function findNeedle(array $words): string
     {
-        $needleIndex = 0;
-
-        for ($i = 0; $i < count($words); $i++) {
-            if ($words[$i] === 'needle') {
-                $needleIndex = $i;
-            }
-        }
-
-        return 'found the needle at position ' . $needleIndex;
+        return "found the needle at position " . array_search("needle", $words);
     }
 }
